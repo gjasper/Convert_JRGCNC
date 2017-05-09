0d2a27bb8d24352fc064d3333e45e888ec1b009f
gjasper <gustavohj6@gmail.com>
2017-05-09 19:37:32 -0300

    Ignored CAD files, and DOCs Added polyline model

---
+++ src/main/java/br/com/jaraguacnc/utils/GC.java
@@ -0,0 +1,15 @@
+package br.com.jaraguacnc.utils;
+
+public class GC {
+
+	public static final int ZERO = 0;
+	public static final int COMMENT = 999;
+	public static final int ATTRIBUTE = 2;
+	public static final int PRIMARY_X = 10;
+	public static final int PRIMARY_Y = 20;
+	public static final int PRIMARY_Z = 30;
+	public static final int RADIUS = 40;
+	public static final int LAYER = 8;
+	public static final int LAYER_COLOR = 62;
+	
+}
